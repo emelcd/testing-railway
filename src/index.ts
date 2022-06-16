7cc178e42998b2b072d139ab79fad51136d7e4d5
emelcd <mick.altura@gmail.com>
2022-06-16 23:36:57 +0200

    ECO

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from './server'
 import './config/db.config'
+import 'dotenv/config'
 
-app.listen(3000, () =>
+const PORT = process.env.PORT
+
+app.listen(PORT, () =>
   console.log('Server started on port http://localhost:3000')
 )
