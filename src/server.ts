a0b86c2a42c76acbe78c84159a6d2809b647a5d9
emelcd <mick.altura@gmail.com>
2022-06-17 02:17:45 +0200

    List endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import apiRouter from './routes'
+import helmet from 'helmet'
+import listEndpoints from 'express-list-endpoints'
 
 const app: express.Express = express()
 app.use(express.json())
@@ -9,7 +11,11 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(cors())
 app.use(morgan('dev'))
+app.use(helmet())
 
 app.use('/api', apiRouter)
+app.use('/', (req, res) => {
+  res.send(listEndpoints(app))
+})
 
 export default app
